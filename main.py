9cc13bb3537e03259dce19e214b4ca32af98d3fc
domenicodatri88 <domenicodatri88@gmail.com>
2026-09-17 10:25:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,87 @@
-# This is a sample Python script.
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+player = {
+    "name": "",
+    "health": 100,
+    "max_health": 100,
+    "attack": 20,
+    "defense": 10
+}
 
+enemy = {
+    "name": "Goblin",
+    "health": 80,
+    "max_health": 80,
+    "attack": 15,
+    "defense": 8
+}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+actions = ["Attack", "Heal"]
 
+def initialize_player(player_stat):
+    user_name = input("Insert your name: ")
+    player_stat["name"] = user_name
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def combat_system_player(player, enemy):
+    action = input("Pick one option, Attack (A) or Heal (H): ").lower()
+    if action == "a":
+        damage = player["attack"] - enemy["defense"]
+        if damage < 0:
+            damage = 0
+        print(f"Player attacks with strength = {player['attack']}")
+        enemy["health"] -= damage
+        if enemy["health"] < 0:
+            enemy["health"] = 0
+        print(f"Enemy health: {enemy['health']}")
+    elif action == "h":
+        player["health"] += 10
+        if player["health"] > player["max_health"]:
+            player["health"] = player["max_health"]
+        print(f"Player health: {player['health']}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def combat_system_enemy(player, enemy, actions):
+    action = random.choice(actions)
+    if action == "Attack":
+        print(f"Enemy decided to {action}")
+        damage = enemy["attack"] - player["defense"]
+        if damage < 0:
+            damage = 0
+        player["health"] -= damage
+        if player["health"] < 0:
+            player["health"] = 0
+        print(f"Player health: {player['health']}")
+    elif action == "Heal":
+        print(f"Enemy decided to {action}")
+        enemy["health"] += 10
+        if enemy["health"] > enemy["max_health"]:
+            enemy["health"] = enemy["max_health"]
+        print(f"Enemy health: {enemy['health']}")
+
+game_is_on = True
+while game_is_on:
+    player["health"] = player["max_health"]
+    enemy["health"] = enemy["max_health"]
+
+    print("Welcome to the battle. Here the challengers of today!\n")
+    initialize_player(player)
+    for key in player:
+        print(f"{key}: {player[key]}")
+    print("\nAND\n")
+    for key in enemy:
+        print(f"{key}: {enemy[key]}")
+
+    combat_round = True
+    while combat_round:
+        combat_system_player(player, enemy)
+        if enemy["health"] <= 0:
+            print("Enemy is dead, congratulations! YOU WIN")
+            combat_round = False
+        else:
+            combat_system_enemy(player, enemy, actions)
+            if player["health"] <= 0:
+                print("You died!")
+                combat_round = False
+
+    keep_going = input("Do you want to play another round? Type Y or N: ").lower()
+    if keep_going == "n":
+        game_is_on = False
